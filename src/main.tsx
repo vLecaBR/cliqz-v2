322495203d8cb5f30444e99f5e34bed5e0ee548e
vlecabr <vitartasleca@gmail.com>
2025-11-19 10:46:41 -0300

    tailwind funcionando

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 
-import "./styles/globals.css"; // SEMPRE primeiro (Tailwind)
-import "./index.css";          // ajustes
-import "./App.css";            // opcional
+// Importe APENAS o tailwind.css
+import "./styles/tailwind.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
